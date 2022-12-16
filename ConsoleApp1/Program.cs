77d0c93e58377bd805a033a9deb3c160682fc3a5
leonidshlyakhtin <117947233+leonidshlyakhtin@users.noreply.github.com>
2022-12-16 09:51:35 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение радиуса: ");
+            double R = double.Parse(Console.ReadLine());
+            Console.WriteLine("Длина окружности = {0}", 2 * Math.PI * R);
+            Console.WriteLine("Площадь круга = {0}", Math.PI * Math.Pow(R, 2));
+            Console.ReadKey();
+        }
+    }
 }
